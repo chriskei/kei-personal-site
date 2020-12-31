eff8231bbc38a38912d77de341e88e4be0527b9c
chriskei <kei.c@husky.neu.edu>
2020-12-31 11:44:41 -0500

    feat: initial threejs test

--- components/hello-section.jsx
+++ components/hello-section.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Canvas } from 'react-three-fiber';
 
 import Section from './section';
 
 const HelloSection = (props) => {
   const { changeSection } = props;
+  const [light, setLight] = useState(10);
 
   return (
     <Section id="hello" changeSection={changeSection}>
       <p>Hello section</p>
+      <div style={{ height: 500, width: 500, background: 'green' }}>
+        <Canvas>
+          <pointLight position={[10, light, 10]} />
+          <mesh>
+            <sphereBufferGeometry />
+            <meshStandardMaterial color="hotpink" />
+          </mesh>
+        </Canvas>
+      </div>
+      <button onClick={() => setLight((light + 10) % 30)}>click</button>
     </Section>
   );
 };
